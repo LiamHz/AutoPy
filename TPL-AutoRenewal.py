9b470b4fa2ec3a894242f6e3edf1fc6f28b85a6c
Liam Hinzman <liam.hinzman@gmail.com>
2018-08-08 14:42:28 -0400

    Changed item due date to item status

--- TPL-AutoRenewal.py
+++ TPL-AutoRenewal.py
@@ -56,21 +56,24 @@ while userIndex < numUsers:
     itemIndex = 1
 
     while renewing:
-        itemDueDateXPath = "//table[@class='item-list']/tbody[{}]/tr[@class='item-row']/td[@class='hidden-mobile']/div[@class='item-due']".format(itemIndex)
-        renewButtonXPath = "//table[@class='item-list']/tbody[{}]/tr[@class='item-row']/td[@class='item-actions']/button".format(itemIndex)
+        itemStatusXPath = "//table[@class='item-list']/tbody[{}]//div[@class='item-status']".format(itemIndex)
+        renewButtonXPath = "//table[@class='item-list']/tbody[{}]//button".format(itemIndex)
 
         # Wait until checkouts page loads
         elem = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, itemDueDateXPath))
+            EC.presence_of_element_located((By.XPATH, itemStatusXPath))
         )
-        elem = driver.find_element_by_xpath(itemDueDateXPath)
+        elem = driver.find_element_by_xpath(itemStatusXPath)
+        elemtext = elem.text.lower()
 
         # If checkout is due today or tomorrow renew hold
-        if 'tomorrow' in elem.text or 'today' in elem.text:
+        if 'tomorrow' in elemtext or 'today' in elemtext or 'overdue' in elemtext:
             elem = driver.find_element_by_xpath(renewButtonXPath)
             elem.click()
             itemIndex += 1
         else:
             renewing = False
+
+        print(renewing)
 
     userIndex += 1
