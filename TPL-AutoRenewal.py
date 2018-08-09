c1dbf95ad864eaa50afe48f19a1461a002672d22
Liam Hinzman <liam.hinzman@gmail.com>
2018-08-08 22:14:24 -0400

    Error Checking; Emails user if item could not be renewed

--- TPL-AutoRenewal.py
+++ TPL-AutoRenewal.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#Enable waiting
+
+# Enable waiting
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+# Enable excpetions
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
+# Enable emailing
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 numUsers = 0
 searching = True
@@ -23,17 +32,23 @@ while searching:
     except IndexError:
         searching = False
 
-# Each user has a pair of credentials
-numUsers = numUsers / 2
+# Each user has a trio of credentials
+numUsers = numUsers / 3
 
 userIndex = 0
 USERNAME = []
 PASSWORD = []
 
+EMAIL_USERNAME = lines[1]
+EMAIL_PASSWORD = lines[2]
+
+TOADDR = []
+
 # Read user credentials from external file and add them to arrays
 while userIndex < numUsers:
-    USERNAME.append(lines[17 + 2 * userIndex])
-    PASSWORD.append(lines[18 + 2 * userIndex])
+    TOADDR.append(lines[17 + 3 * userIndex])
+    USERNAME.append(lines[18 + 3 * userIndex])
+    PASSWORD.append(lines[19 + 3 * userIndex])
     userIndex += 1
 f.close()
 
@@ -53,27 +68,75 @@ while userIndex < numUsers:
     elem.send_keys(Keys.RETURN)
 
     renewing = True
+    itemsRenewed = []
     itemIndex = 1
 
     while renewing:
+        itemTitleXPath = "//table[@class='item-list']/tbody[{}]//div[@class='item-title']".format(itemIndex)
         itemStatusXPath = "//table[@class='item-list']/tbody[{}]//div[@class='item-status']".format(itemIndex)
         renewButtonXPath = "//table[@class='item-list']/tbody[{}]//button".format(itemIndex)
 
         # Wait until checkouts page loads
-        elem = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, itemStatusXPath))
-        )
+        # If itemStatusXPath is not found it's most likely because
+        # it's trying to access past the end of the checkout list
+        try:
+            elem = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, itemStatusXPath))
+            )
+        except TimeoutException:
+            renewing = False
+            break
+
         elem = driver.find_element_by_xpath(itemStatusXPath)
         elemtext = elem.text.lower()
 
         # If checkout is due today or tomorrow renew hold
         if 'tomorrow' in elemtext or 'today' in elemtext or 'overdue' in elemtext:
-            elem = driver.find_element_by_xpath(renewButtonXPath)
-            elem.click()
+
+            # Add matching item to list showing renewal has been attempted
+            elem = driver.find_element_by_xpath(itemTitleXPath)
+            itemsRenewed.append(elem.text)
+
+            # Test if renew button is available
+            try:
+                elem = driver.find_element_by_xpath(renewButtonXPath)
+                elem.click()
+            except NoSuchElementException:
+                print("No Renew Button!")
+
             itemIndex += 1
         else:
             renewing = False
 
-        print(renewing)
+    itemsFailedRenewed = []
+
+    # Check if any checkouts failed to renew
+    for title in itemsRenewed:
+        if title in driver.page_source:
+            itemsFailedRenewed.append(title)
+
+    # If any results failed to renew email them to user
+    if len(itemsFailedRenewed) >= 1:
+        s = "\n"
+        formattedItemsFailedRenewed = s.join(itemsFailedRenewed)
+
+        addr = EMAIL_USERNAME
+        toaddr = TOADDR[userIndex]
+        msg = MIMEMultipart()
+        msg['From'] = addr
+        msg['To'] = toaddr
+        msg['Subject'] = "TPL AutoRenewal: The following items could not be renewed"
+
+        # Allow Unicode characters to be emailed
+        text = MIMEText(formattedItemsFailedRenewed.encode('utf-8'), 'plain', 'UTF-8')
+
+        msg.attach(text)
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(addr, EMAIL_PASSWORD)
+        print("Sent email to {}".format(toaddr))
+        server.sendmail(addr, toaddr, msg.as_string())
+        server.quit()
 
     userIndex += 1
